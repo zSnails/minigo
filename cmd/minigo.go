4e356b385dffa7e6101708371d4d8602aa6410f6
zSnails <erizojuan33@gmail.com>
2024-03-30 23:36:19 -0600

    feat: add custom error listener

--- cmd/minigo.go
+++ cmd/minigo.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/antlr4-go/antlr/v4"
 	"github.com/zSnails/minigo/grammar"
+	"github.com/zSnails/minigo/reporter"
 )
 
 func main() {
@@ -15,7 +17,17 @@ func main() {
 	lexer := grammar.NewMinigoLexer(fileStream)
 	cts := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
 	parser := grammar.NewMinigoParser(cts)
+	r := reporter.NewReporter(fileStream.GetSourceName())
+	parser.RemoveErrorListeners()
+	parser.AddErrorListener(r)
 	ctx := parser.Root()
+	if r.HasErrors() {
+		errs := r.GetErrors()
+		for _, err := range errs {
+			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		}
+		return
+	}
 	s := ctx.ToStringTree(nil, parser)
 	fmt.Printf("s: %v\n", s)
 }
---
+++ reporter/reporter.go
@@ -0,0 +1,34 @@
+package reporter
+
+import (
+	"fmt"
+
+	"github.com/antlr4-go/antlr/v4"
+)
+
+type MinigoReporter struct {
+	antlr.DefaultErrorListener
+	errors   []error
+	fileName string
+}
+
+// SyntaxError implements antlr.ErrorListener.
+func (m *MinigoReporter) SyntaxError(recognizer antlr.Recognizer, offendingSymbol interface{}, line int, column int, msg string, e antlr.RecognitionException) {
+	m.errors = append(m.errors, fmt.Errorf("%s:%d:%d: %s", m.fileName, line, column, msg))
+}
+
+func (m *MinigoReporter) GetErrors() []error {
+	return m.errors
+}
+
+func (m *MinigoReporter) HasErrors() bool {
+	return len(m.errors) > 0
+}
+
+func NewReporter(filename string) *MinigoReporter {
+	return &MinigoReporter{
+		DefaultErrorListener: antlr.DefaultErrorListener{},
+		errors:               []error{},
+		fileName:             filename,
+	}
+}
