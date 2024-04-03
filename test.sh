ff0264c2de1a31210d0afc8be91a3d5130b12547
zSnails <erizojuan33@gmail.com>
2024-04-02 19:49:31 -0600

    feat: add extensive test suite

---
+++ test.sh
@@ -0,0 +1,12 @@
+FILES=$(ls tests/)
+
+for file in $FILES; {
+    echo running $file
+    ./minigo -file tests/$file > /dev/null 2>&1
+    execution_status=$?
+    type=$(echo $file | rg -q error)
+    if [[ $execution_status -eq $? ]]; then
+        echo unexpected status while running $file check its contents
+        exit
+    fi
+}
