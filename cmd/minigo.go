674335fa2256f8f207c2788705403e8de3f802f3
zSnails <erizojuan33@gmail.com>
2024-04-27 20:18:13 -0600

    chore: remove old debug print statements

--- cmd/minigo.go
+++ cmd/minigo.go
@@ -62,7 +62,6 @@ func main() {
 		fmt.Fprintf(os.Stderr, "%s", r.String())
 		os.Exit(CompilerError)
 	}
-	// fmt.Printf("ctx.ToStringTree(): %v\n", ctx.ToStringTree(nil, parser))
 
 	typeChecker := checker.NewTypeChecker(fileStream.GetSourceName(), r.(antlr.ErrorListener))
 	typeChecker.Visit(ctx)
@@ -70,5 +69,4 @@ func main() {
 		fmt.Fprintf(os.Stderr, "%s", r.String())
 		os.Exit(CompilerError)
 	}
-	fmt.Printf("typeChecker.SymbolTable: %v\n", typeChecker.SymbolTable)
 }
