43b89d2c0c878e923918cb1ead419dd0cae79dcf
zSnails <erizojuan33@gmail.com>
2024-05-20 23:19:34 -0600

    feat: implement for and break semantics

--- backend/llvm/llvm.go
+++ backend/llvm/llvm.go
@@ -509,9 +509,45 @@ func (l *LlvmBackend) VisitIndex(ctx *grammar.IndexContext) interface{} {
 	panic("unimplemented")
 }
 
+// VisitBreakStatement implements grammar.MinigoVisitor.
+func (l *LlvmBackend) VisitBreakStatement(ctx *grammar.BreakStatementContext) interface{} {
+	blk, _ := l.blockStack.Peek()
+	loop, err := l.loopStack.Pop()
+    if err != nil {
+        panic(err)
+    }
+	blk.NewBr(loop)
+	return nil
+}
+
 // VisitInfiniteFor implements grammar.MinigoVisitor.
 func (l *LlvmBackend) VisitInfiniteFor(ctx *grammar.InfiniteForContext) interface{} {
-	panic("unimplemented")
+	blk, _ := l.blockStack.Peek()
+	fn, _ := l.funcStack.Peek()
+
+	_for := fn.NewBlock(fmt.Sprintf("start%d", l.blockStack.Len()))
+	end := ir.NewBlock(fmt.Sprintf("end%d", l.blockStack.Len()))
+
+	blk.NewBr(_for)
+
+	l.blockStack.Push(_for)
+	l.loopStack.Push(end)
+
+	l.Visit(ctx.Block())
+
+	got, _ := l.blockStack.Peek()
+	if got.Term == nil {
+		got.NewBr(_for)
+	}
+
+	// l.loopStack.Peek()
+
+	end.Parent = fn.Func
+	fn.Blocks = append(fn.Blocks, end)
+
+	l.blockStack.Push(end)
+
+	return nil
 }
 
 // VisitInnerTypeDecls implements grammar.MinigoVisitor.
