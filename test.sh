aef28219f2e2bd77ee49976987c121aedd37eb14
zSnails <erizojuan33@gmail.com>
2024-05-31 15:27:14 -0600

    refactor: write more tests

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ FILES=$(ls tests/)
 
 for file in $FILES; {
     echo running $file
-    ./minigo -file tests/$file > /dev/null 2>&1
+    ./minigo run tests/$file > /dev/null 2>&1
     execution_status=$?
     type=$(echo $file | rg -q error)
     if [[ $execution_status -eq $? ]]; then
