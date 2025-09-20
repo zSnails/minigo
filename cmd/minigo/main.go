295ddce28fd2162185fe187e26d0c8a5776afaf1
zSnails <erizojuan33@gmail.com>
2025-09-20 14:03:03 -0600

    fix: properly format the entry point

--- cmd/minigo/main.go
+++ cmd/minigo/main.go
@@ -17,18 +17,14 @@ import (
 
 const (
 	CompilerError = iota + 1
-
 	InternalError
-
 	ExternalError
 )
 
 var tmp = os.TempDir()
 
 func getFileName(input string) string {
-
 	return strings.TrimSuffix(input, filepath.Ext(input))
-
 }
 
 func main() {
@@ -43,171 +39,101 @@ func main() {
 		WithOption(json).
 		WithArg(cli.NewArg("file", "the source file to be built and run")).
 		WithAction(func(args []string, options map[string]string) int {
-
 			code := build(args, options)
-
 			if code != 0 {
-
 				return code
-
 			}
-
 			filename := path.Clean(args[0])
-
 			executable := getFileName(path.Base(filename))
-
 			return run(executable)
-
 		}))
 
 	code := app.Run(os.Args, os.Stdout)
-
 	os.Exit(code)
-
 }
 
 func run(executable string) int {
-
 	path, err := filepath.Abs(executable)
-
 	if err != nil {
-
 		fmt.Fprintln(os.Stderr, err)
-
 		return InternalError
-
 	}
 
 	cmd := exec.Command(path + EXTENSION)
-
 	cmd.Stdout = os.Stdout
-
 	cmd.Stderr = os.Stderr
-
 	cmd.Stdin = os.Stdin
 
 	if err := cmd.Run(); err != nil {
-
 		fmt.Fprintln(os.Stderr, err)
-
 		return ExternalError
-
 	}
 
 	return 0
-
 }
 
 func build(args []string, options map[string]string) int {
-
 	filename := path.Clean(args[0])
-
 	fileStream, err := antlr.NewFileStream(filename)
-
 	if err != nil {
-
 		fmt.Fprintln(os.Stderr, err)
-
 		return InternalError
-
 	}
 
 	lexer := grammar.NewMinigoLexer(fileStream)
-
 	lexer.RemoveErrorListeners()
-
 	cts := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
-
 	parser := grammar.NewMinigoParser(cts)
-
 	var r reporter.Reporter
-
 	parser.RemoveErrorListeners()
-
 	_, json := options["json"]
-
 	if json {
-
 		r = reporter.NewJsonReporter(fileStream.GetSourceName())
-
 	} else {
-
 		r = reporter.NewReporter(fileStream.GetSourceName())
-
 	}
 
 	listener, ok := r.(antlr.ErrorListener)
-
 	if !ok {
-
 		return InternalError
-
 	}
 
 	lexer.AddErrorListener(listener)
-
 	parser.AddErrorListener(listener)
-
 	ctx := parser.Root()
-
 	if r.HasErrors() {
-
 		fmt.Fprintln(os.Stderr, r.String())
-
 		return CompilerError
-
 	}
 
 	typeChecker := checker.NewTypeChecker(fileStream.GetSourceName(), listener)
-
 	typeChecker.Visit(ctx)
-
 	if r.HasErrors() {
-
 		fmt.Fprintln(os.Stderr, r.String())
-
 		return CompilerError
-
 	}
 
 	backend := llvm.NewLlvmBackend(listener)
-
 	backend.Visit(ctx)
-
 	if r.HasErrors() {
-
 		fmt.Fprintln(os.Stderr, r.String())
-
 		return CompilerError
-
 	}
 
 	backend.GetModule().SourceFilename = filename
-
 	base := filepath.Base(filename)
-
 	out, err := os.CreateTemp(tmp, base+"*.ll")
-
 	if err != nil {
-
 		fmt.Fprintln(os.Stderr, err)
-
 		return CompilerError
-
 	}
 
 	defer func() {
-
 		err := os.Remove(out.Name())
-
 		if err != nil {
-
 			fmt.Fprintln(os.Stderr, err)
-
 			os.Exit(CompilerError)
-
 		}
-
 	}()
 
 	defer out.Close()
@@ -228,5 +154,4 @@ func build(args []string, options map[string]string) int {
 	}
 
 	return 0
-
 }
